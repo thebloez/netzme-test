f4b3b611a3239ed05a55429541060200286f1559
thebloez <ryandunksfamz@gmail.com>
2019-10-30 09:56:44 +0700

    remove class response for more lite

--- src/main/java/com/netzme/test/model/ResponseRetrofit.java
+++ src/main/java/com/netzme/test/model/ResponseRetrofit.java
@@ -10,9 +10,6 @@ public class ResponseRetrofit {
     @SerializedName("results")
     @Expose
     private List<Result> results = null;
-    @SerializedName("info")
-    @Expose
-    private Info info;
 
     public List<Result> getResults() {
         return results;
@@ -20,13 +17,5 @@ public class ResponseRetrofit {
 
     public void setResults(List<Result> results) {
         this.results = results;
-    }
-
-    public Info getInfo() {
-        return info;
-    }
-
-    public void setInfo(Info info) {
-        this.info = info;
     }
 }
--- src/main/java/com/netzme/test/model/Result.java
+++ src/main/java/com/netzme/test/model/Result.java
@@ -14,33 +14,9 @@ public class Result {
     @SerializedName("location")
     @Expose
     private Location location;
-    @SerializedName("email")
-    @Expose
-    private String email;
-    @SerializedName("login")
-    @Expose
-    private Login login;
-    @SerializedName("dob")
-    @Expose
-    private DOB dob;
-    @SerializedName("registered")
-    @Expose
-    private Registered registered;
-    @SerializedName("phone")
-    @Expose
-    private String phone;
-    @SerializedName("cell")
-    @Expose
-    private String cell;
-    @SerializedName("id")
-    @Expose
-    private Id id;
     @SerializedName("picture")
     @Expose
     private Picture picture;
-    @SerializedName("nat")
-    @Expose
-    private String nat;
 
     public String getGender() {
         return gender;
@@ -66,75 +42,11 @@ public class Result {
         this.location = location;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Login getLogin() {
-        return login;
-    }
-
-    public void setLogin(Login login) {
-        this.login = login;
-    }
-
-    public DOB getDob() {
-        return dob;
-    }
-
-    public void setDob(DOB dob) {
-        this.dob = dob;
-    }
-
-    public Registered getRegistered() {
-        return registered;
-    }
-
-    public void setRegistered(Registered registered) {
-        this.registered = registered;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCell() {
-        return cell;
-    }
-
-    public void setCell(String cell) {
-        this.cell = cell;
-    }
-
-    public Id getId() {
-        return id;
-    }
-
-    public void setId(Id id) {
-        this.id = id;
-    }
-
     public Picture getPicture() {
         return picture;
     }
 
     public void setPicture(Picture picture) {
         this.picture = picture;
-    }
-
-    public String getNat() {
-        return nat;
-    }
-
-    public void setNat(String nat) {
-        this.nat = nat;
     }
 }
